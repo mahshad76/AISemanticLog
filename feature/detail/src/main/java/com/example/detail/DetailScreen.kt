b3c304aee2f8ed6adfdae4decc4c8b4190cb0ef2
mahshad mahdavi <mahshad@polarismail.com>
2026-07-12 00:23:34 -0400

    A simple detail screen for test is added

--- feature/detail/src/main/java/com/example/detail/DetailScreen.kt
+++ feature/detail/src/main/java/com/example/detail/DetailScreen.kt
@@ -1,11 +1,58 @@
 package com.example.detail
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.detail.navigation.DetailRoute
 import com.example.detail.navigation.DetailUiModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DetailScreen(uiModel: DetailUiModel, onBackClick: () -> Unit) {
-    Text(uiModel.metadata)
+internal fun DetailScreen(
+    uiModel: DetailUiModel,
+    onBackClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = { },
+                navigationIcon = {
+                    IconButton(onClick = onBackClick) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = null
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                )
+            )
+        }
+    ) { padding ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) {
+            item {
+                Text(uiModel.message)
+            }
+        }
+    }
 }
